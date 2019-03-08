ee6d862a7cc20ad6015d495a23201aaf3d71833d
Thomas Mansencal <thomas.mansencal@gmail.com>
2019-03-09 12:33:52 +1300

    Update copyright statements.

--- colour_spectroscope/fraunhofer/analysis.py
+++ colour_spectroscope/fraunhofer/analysis.py
@@ -21,7 +21,7 @@ from colour import (Extrapolator, LinearInterpolator, RGB_COLOURSPACES,
 from colour.utilities import tstack
 
 __author__ = 'Colour Developers'
-__copyright__ = 'Copyright (C) 2013-2018 - Colour Developers'
+__copyright__ = 'Copyright (C) 2013-2019 - Colour Developers'
 __license__ = 'New BSD License - http://opensource.org/licenses/BSD-3-Clause'
 __maintainer__ = 'Colour Developers'
 __email__ = 'colour-science@googlegroups.com'
